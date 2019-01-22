8411c6d2a3c416b3137c1494240833daf1a7e661
shunAoki0109 <shunaoki0000@gmail.com>
2019-01-22 18:03:04 +0900

    test app add

---
+++ Pokilib/pokilib2/src/main/java/jp/pokepay/pokilib2/PokiActivity.java
@@ -0,0 +1,13 @@
+package jp.pokepay.pokilib2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PokiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_poki);
+    }
+}
